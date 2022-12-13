3d14b10c972fd8ba749217ab378b2ec5300c831f
lbroekho3071 <luukbroekhof@icloud.com>
2022-12-13 21:35:46 +0100

    create index endpoint to serve index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main()
+{
+    let index = warp::path::end().and(warp::fs::dir("www/static"));
+
+    warp::serve(index).run(([127, 0, 0, 1], 3000)).await;
 }
